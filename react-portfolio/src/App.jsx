9060c07b325942ddd28ef3c3789deaf91a4cbc5a
grantwilson1 <grantvwilson@icloud.com>
2024-12-12 14:19:17 -0500

    a

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,10 +1,14 @@
 import styles from './App.module.css'
+import { Navbar } from './components/navbar/Navbar'
+
 
 function App() {
 
   return (
-    <div className={styles.App}>Hello world!</div>
-  )
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App
