e64dd7e17c681543a120f5b3a2be0535367373ec
grantwilson1 <grantvwilson@icloud.com>
2024-12-17 23:11:25 -0500

    a

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,29 +1,30 @@
 import React from "react";
-import styles from "./About/About.modules.css";
+import styles from "./About.module.css";
+import student from "../../../assets/about/graduated.png";
 
 export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img src={getImageUrl()} alt="me" className={styles.aboutImage} />
+        <img src={me} alt="me" className={styles.aboutImage} />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl} alt="cursor icon" />
+            <img src={student} alt="cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend dev</h3>
               <p>Im a frontend....</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl} alt="cursor icon" />
+            <img src={me} alt="cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend dev</h3>
               <p>Im a frontend....</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl} alt="cursor icon" />
+            <img src={me} alt="cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend dev</h3>
               <p>Im a frontend....</p>
