67553c3d71773840477c376cb3e82d5807cc59cc
grantwilson1 <grantvwilson@icloud.com>
2024-12-17 23:28:19 -0500

    a

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import styles from "./About.module.css";
 import student from "../../../assets/about/graduated.png";
+import hobby from "../../../assets/about/hobby.png";
 
 export const About = () => {
   return (
     <section className={styles.container} id="about">
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
-        <img src={me} alt="me" className={styles.aboutImage} />
+        <img src={student} alt="me" className={styles.aboutImage} />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
             <img src={student} alt="cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend dev</h3>
+              <h3>Student</h3>
               <p>Im a frontend....</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={me} alt="cursor icon" />
+            <img src={hobby} alt="cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend dev</h3>
+              <h3>Hobbies</h3>
               <p>Im a frontend....</p>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={me} alt="cursor icon" />
+            <img src={student} alt="cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend dev</h3>
               <p>Im a frontend....</p>
