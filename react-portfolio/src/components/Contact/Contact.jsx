ff624d15267bc4eadf3a4f7a22dffd0c944fbe5f
grantwilson1 <grantvwilson@icloud.com>
2024-12-20 20:12:46 -0500

    a

--- react-portfolio/src/components/Contact/Contact.jsx
+++ react-portfolio/src/components/Contact/Contact.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:grantvwilson@icloud.com">grantvwilson@icloud.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="linkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/grant-wilson12/">
+            linkedin.com/grant-wilson12
+          </a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="github icon" />
+          <a href="https://github.com/grantwilson1">github.com/grantwilson1</a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./Projects.module.css";
 
 import projects from "../../data/projects.json";
-import { ProjectCard } from "./ProjectCard";
+import ProjectCard from "./ProjectCard";
 
 export const Projects = () => {
   return (
